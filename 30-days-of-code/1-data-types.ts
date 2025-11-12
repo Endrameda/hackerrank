9063fd1f16c42e1d109a74548d87027a452e92a7
Sardor Gofurov <endrameda98@gmail.com>
2025-11-12 22:20:44 +0500

    1st day and add gitignore with nodejs types

---
+++ 30-days-of-code/1-data-types.ts
@@ -0,0 +1,35 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function (inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function (): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+function main() {
+    const i: number = 4;
+    const d: number = 4.0;
+    const s: string = "HackerRank ";
+
+    const i2: number = parseInt(readLine());
+    const d2: number = parseFloat(readLine());
+    const s2: string = readLine();
+
+    console.log(i + i2);
+    console.log((d + d2).toFixed(1));
+    console.log(s + s2);
+}
